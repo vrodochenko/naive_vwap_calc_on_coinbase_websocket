e2b5f6ed9bc4780cbb870fc4f3188bd7ce988edd
vrodochenko <rodochenko@spark-mail.ru>
2021-12-23 21:05:05 +0300

    Add docs for with_event_hooks

--- app/with_event_hooks.py
+++ app/with_event_hooks.py
@@ -1,21 +1,36 @@
 import logging
+from typing import Any
 
 logger = logging.getLogger(__name__)
 
 
 class WithEventHooksMixin:
-    def __init__(self, *args, **kwargs):
+    """A way to attach hooks to some events.
+
+    Can be used for testing/debuggging or doing actual job"""
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize and all heavy lifting to the superclass.
+
+        :param args: args
+        :param kwargs: kwargs"""
+
         super().__init__(*args, **kwargs)
 
-    def on_open(self):
-        logger.debug("subscribed")
+    def on_open(self) -> None:
+        """Run on_open hooks, if any."""
+        logger.debug("on_open called")
 
-    def on_close(self):
-        logger.debug("closed")
+    def on_close(self) -> None:
+        """Run on_close hooks, if any."""
+        logger.debug("on_close called")
 
-    def on_message(self, msg):
-        logger.debug("message received")
-        logger.debug(msg)
+    def on_message(self, msg: str) -> None:
+        """Run on_message hooks, if any.
 
-    def on_error(self, e):
-        logger.exception(e)
+        :param msg: a message received by the subclass
+        """
+        logger.debug(f"message {msg} received")
+
+    def on_error(self, e: Exception) -> None:
+        """Run on_error hooks, if any."""
+        logger.error(e)
